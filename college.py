c09cc663ad0c67361d537b2c24fad61d5f7e4865
AleenaShibu <54793714+AleenaShibu@users.noreply.github.com>
2019-09-04 08:36:43 +0530

    Add files via upload

---
+++ college.py
@@ -0,0 +1,27 @@
+class college:
+    def __init__(self,name,department):
+        self.name = name
+        self.department = department
+    def print_details(self):
+        print(f"{self.name},{self.department}")
+class student(college):
+    def __init__(self,name,department,roll_no):
+        college.__init__(self,name,department)
+        self.rollno = roll_no
+    def prinit_details(self):
+        college.print_details(self)
+        print(f"{self.roll_no}")
+
+p = student("Shalini","Chemistry",3)
+p.print_details()
+class teacher(college):
+    def __init__(self,name,deparment,college_id):
+        college.__init__(self,name,department)
+        self.college_id = college_id
+
+    def print_details(self):
+        college.print_details(self)
+        print(f"{self,college_id}")
+
+p1 = teacher("Diviya","Chemistry",2013)
+p1.print_detail()
